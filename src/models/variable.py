f09d3f6a8854ca29f815c075bddaaff4a1bc1688
Hicham SADDEK <h.saddek@serrahub.com>
2022-02-22 14:01:44 +0100

    Modification du modèle de variable

--- src/models/variable.py
+++ src/models/variable.py
@@ -3,14 +3,13 @@ import requests
 
 
 class Variable:
-  def __init__(self, id, namespace, identifier):
+  def __init__(self, id, nodeid):
     self.id = int(id)
-    self.namespace = int(namespace)
-    self.identifier = int(identifier)
+    self.nodeid = str(nodeid)
 
   def from_json(json):
-    print(f"[LOG: Models actions] - Model parsed with ({json['id']}, {json['opc_ua_namespace_index']}, {json['opc_ua_identifier']})")
-    return Variable(json['id'], json['opc_ua_namespace_index'], json['opc_ua_identifier'])
+    print(f"[LOG: Models actions] - Model parsed with ({json['id']}, {json['node_id']})")
+    return Variable(json['id'], json['node_id'])
   
   def from_bulk_json(json):
     variables = []
@@ -19,11 +18,11 @@ class Variable:
     return variables
 
   def __str__(self) -> str:
-      return f"OPC-UA Variable {self.id} of ({self.namespace}, {self.identifier})"
+      return f"OPC-UA Variable {self.id} of {self.nodeid})"
 
   @property
   def node_id(self):
-    return f"ns={self.namespace};i={self.identifier}"
+    return self.nodeid
 
   def register(self):
     pass
